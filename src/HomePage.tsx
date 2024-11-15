8685d3b562b1f481ece4d0724f84e7c15eb271ef
Gordon06060 <gordonz0913@gmail.com>
2024-11-14 23:36:08 -0500

    feat: homepage menu

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,6 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Homepage.css";
 
 export default function HomePage() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <div className="menu-container">
+        <div className="logo">BLNDSPT</div>
+        <div className="shop-menu-container">
+          <div className="shop-menu">
+            <Link to="/shop" className="shop-link">
+              SHOP
+            </Link>
+          </div>
+          <div>
+            <Link to="/shop" className="shop-link">
+              ABOUT
+            </Link>
+          </div>
+          <div>
+            <Link to="/shop" className="shop-link">
+              CONTACT
+            </Link>
+          </div>
+          <div>
+            <Link to="/shop" className="shop-link">
+              MAILING LIST
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
