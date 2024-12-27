20988e768ec62116f5f5ad53eca80a3eea247e40
anshm42 <69057488+anshm42@users.noreply.github.com>
2024-12-26 19:48:50 -0500

    finished problem w shop page and navbar. also fuck commit msgs

--- src/Shop.tsx
+++ src/Shop.tsx
@@ -1,15 +1,24 @@
 export default function Shop() {
   return (
+    // smths fucky w this div
     <div
       style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
+        // display: "flex",
+        // justifyContent: "center",
+        // alignItems: "center",
+
+        // this is fucking w the navbar , why? i dont know
+        // position: "absolute",
+        // top: 0,
+        // left: 0,
+        // right: 0,
+        // bottom: 0,
+        
+        // this works idek
         position: "absolute",
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
+        left: "50%",
+        top: "50%",
+        transform: "translate(-50%, -50%)",
       }}
     >
       <div style={{ textAlign: "left" }}>
