f5fdc1d4dfcc450c8d0fc1738c644b3f0b0264c3
Gordon06060 <gordonz0913@gmail.com>
2024-12-29 13:10:57 -0800

    feat: shop boilerplate

--- src/Shop.tsx
+++ src/Shop.tsx
@@ -1,30 +1,72 @@
+import Header from "./NavbarShop";
+import item1 from "./assets/demo.png";
+import item2 from "./assets/ig.png";
+import Footer from "./Footer";
+import "./Shop.css";
+import { useState } from "react";
+
 export default function Shop() {
+  const [hover1, setHover1] = useState(true);
+  const [hover2, setHover2] = useState(true);
+  const [hover3, setHover3] = useState(true);
+  const [hover4, setHover4] = useState(true);
+
   return (
-    <div
-      style={{
-        // display: "flex",
-        // justifyContent: "center",
-        // alignItems: "center",
-
-        // position: "absolute",
-        // top: 0,
-        // left: 0,
-        // right: 0,
-        // bottom: 0,
-
-        position: "absolute",
-        left: "50%",
-        top: "50%",
-        transform: "translate(-50%, -50%)",
-      }}
-    >
-      <div style={{ textAlign: "left" }}>
-        <button>
-          <img src="../public/test.jpg" alt="my image" />
-          <br />
-          SHIRT $190 000
-        </button>
+    <>
+      <Header />
+      <div className="shop-con">
+        <div className="items-con">
+          <div className="item-con-1">
+            <img
+              className="item-png"
+              src={hover1 ? item1 : item2}
+              onMouseEnter={() => setHover1(false)}
+              onMouseLeave={() => setHover1(true)}
+            ></img>
+            <div className="item-desc">
+              <div className="itemtitle">GORDONS HOODIE</div>
+              <div className="item-price">$130</div>
+            </div>
+          </div>
+          <div className="item-con-1">
+            <img
+              className="item-png"
+              src={hover2 ? item1 : item2}
+              onMouseEnter={() => setHover2(false)}
+              onMouseLeave={() => setHover2(true)}
+            ></img>{" "}
+            <div className="item-desc">
+              <div className="itemtitle">GORDONS HOODIE</div>
+              <div className="item-price">$130</div>
+            </div>
+          </div>
+          <div className="item-con-1">
+            <img
+              className="item-png"
+              src={hover3 ? item1 : item2}
+              onMouseEnter={() => setHover3(false)}
+              onMouseLeave={() => setHover3(true)}
+            ></img>{" "}
+            <div className="item-desc">
+              <div className="itemtitle">GORDONS HOODIE</div>
+              <div className="item-price">$130</div>
+            </div>
+          </div>
+          <div className="item-con-1">
+            <img
+              className="item-png"
+              src={hover4 ? item1 : item2}
+              onMouseEnter={() => setHover4(false)}
+              onMouseLeave={() => setHover4(true)}
+            ></img>{" "}
+            <div className="item-desc">
+              <div className="itemtitle">GORDONS HOODIE</div>
+              <div className="item-price">$130</div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
