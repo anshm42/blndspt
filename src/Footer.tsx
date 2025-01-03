770875ade7ce44187d11c546bf2bc6f83b7bd480
Gordon06060 <gordonz0913@gmail.com>
2025-01-03 14:11:43 -0800

    fix: footer links

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import Email from "./Email";
 import "./Footer.css";
 import IG from "./assets/ig.png";
+import { useNavigate } from "react-router-dom";
 
 export default function Footer() {
   function handleIG() {
     window.location.href = "https://www.instagram.com/blndspt.lab/"; // External navigation
   }
+  const navigate = useNavigate();
   return (
     <>
       <div className="footer-container1">
@@ -18,14 +20,21 @@ export default function Footer() {
         </div>
         <div className="policies">
           <div className="poly1">
-            <div>Refund policy</div>
-            <div>Privacy policy</div>
-            <div>Shipping policy </div>
+            <div className="f1">Refund policy</div>
+            <div className="f1">Shipping policy </div>
+            <div
+              className="f1"
+              onClick={() => {
+                navigate("/contact");
+              }}
+            >
+              Contact
+            </div>
           </div>
           <div className="poly1">
-            <div>Terms of Service</div>
-            <div>Order Support</div>
-            <div>About</div>
+            <div className="f1">Terms of Service</div>
+            <div className="f1">Order Support</div>
+            <div className="f1">About</div>
           </div>
         </div>
         <div className="socials">
