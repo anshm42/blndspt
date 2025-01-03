d5be12f751a784aa474655862f8c4d2e11ba4313
Gordon06060 <gordonz0913@gmail.com>
2025-01-03 13:00:00 -0800

    feat: video background

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,33 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Homepage.css";
+import Video from "./assets/placeholder.mp4";
 
 export default function HomePage() {
   return (
     <>
-      <div className="menu-container">
-        <div className="logo">BLNDSPT+</div>
-        <div className="shop-menu-container">
-          <div className="shop-menu">
-            <Link to="/shop" className="shop-link">
-              SHOP
-            </Link>
-          </div>
+      <div className="video">
+        <video autoPlay loop muted playsInline className="background-video">
+          <source src={Video} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="menu-container">
+          <div className="logo">BLNDSPT+</div>
+          <div className="shop-menu-container">
+            <div className="shop-menu">
+              <Link to="/shop" className="shop-link link">
+                SHOP
+              </Link>
+            </div>
+            <div>
+              <Link to="/about-us" className="contact-link link">
+                ABOUT
+              </Link>
+            </div>
 
-          <div>
-            <Link to="/about-us" className="contact-link">
-              ABOUT
-            </Link>
-          </div>
-          <div>
-            <Link to="/shop" className="shop-link">
-              LOG IN
-            </Link>
-          </div>
-          <div>
-            <Link to="/contact" className="shop-link">
-              CONTACT
-            </Link>
+            <div>
+              <Link to="/contact" className="shop-link link">
+                CONTACT
+              </Link>
+            </div>
           </div>
         </div>
       </div>
