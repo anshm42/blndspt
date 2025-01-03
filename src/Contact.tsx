f1948fc453e2d7b66e5ba04f5b30c40a33973954
Gordon06060 <gordonz0913@gmail.com>
2025-01-03 14:06:33 -0800

    feat: contact page

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -1,3 +1,30 @@
+import NavbarShop from "./NavbarShop";
+import Footer from "./Footer";
+import "./Contact.css";
+
 export default function Contact() {
-  return <div>Contact us!</div>;
+  return (
+    <>
+      <NavbarShop />
+      <div className="contactouter">
+        <div className="contactcon">CONTACT</div>
+        <div className="contactcon">
+          <div className="contactinner">General inquiries:</div>
+          <div>gordonz0913@gmail.com</div>
+          <div className="contactinner">Wholesale inquiries:</div>
+          <div>gordonz0913@gmail.com</div>
+
+          <div className="contactinner">Customer Care:</div>
+          <div>gordonz0913@gmail.com</div>
+
+          <div className="contactinner">Press:</div>
+          <div>gordonz0913@gmail.com</div>
+
+          <div className="contactinner"></div>
+        </div>
+        <div className="contactcon"></div>
+      </div>
+      <Footer />
+    </>
+  );
 }
